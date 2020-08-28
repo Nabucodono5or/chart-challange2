71024dd33d5ba05427d3dd799a786750b5f47545
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-28 11:30:37 -0300

    adicionado fontes aos dados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import { select } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { csv } from "d3-fetch";
+import iconSvg from "../resources/error.svg";
+
+// selectAll(".icon-container").append("svg").attr("width", 50).attr("height", 60);
 
 csv(require("../data/netflix_titles.csv")).then((data) => {
   console.log(data);
