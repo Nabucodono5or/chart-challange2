58c3223e3c7b570d9e09987628ce11a9f610e050
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-09-01 07:49:38 -0300

    inserido cores e posições na page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { csv } from "d3-fetch";
 import iconSvg from "../resources/error.svg";
 
 // selectAll(".icon-container").append("svg").attr("width", 50).attr("height", 60);
+let width = 240;
+let height = 230;
+
+selectAll(".chart-block")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 csv(require("../data/netflix_titles.csv")).then((data) => {
   console.log(data);
