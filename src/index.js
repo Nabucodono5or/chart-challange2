4761a5945857aac8ae4808fbbbc4acf856a6fb05
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-09-01 10:44:11 -0300

    medido os dados de rating e inseridos no html

--- src/index.js
+++ src/index.js
@@ -13,4 +13,41 @@ selectAll(".chart-block")
 
 csv(require("../data/netflix_titles.csv")).then((data) => {
   console.log(data);
+  const ratings = mesuareRatings(data);
+  console.log(ratings);
+  renderMesuare(ratings);
 });
+
+function mesuareRatings(incomingData) {
+  const ratings = [
+    { rating: "TV-Y", total: 0 },
+    { rating: "TV-14", total: 0 },
+    { rating: "TV-MA", total: 0 },
+  ];
+
+  ratings.forEach((r) => {
+    incomingData.forEach((d) => {
+      if (r.rating === d.rating) {
+        r.total++;
+      }
+    });
+  });
+
+  return ratings;
+}
+
+function renderMesuare(incomingData) {
+  selectAll(".numbers")
+    .select("span")
+    .data(incomingData)
+    .each(function (d) {
+      select(this).html(d.rating);
+    });
+
+  selectAll(".numbers")
+    .select("h2")
+    .data(incomingData)
+    .each(function (d) {
+      select(this).html(d.total);
+    });
+}
